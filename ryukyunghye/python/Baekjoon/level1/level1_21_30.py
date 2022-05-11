0c22603a98e5f5beb1c9e1dde9e85bacd5f7a1a8
ryukyunghye <ryukyung123@naver.com>
2022-05-11 22:36:29 +0900

    add: level1_21_24

---
+++ ryukyunghye/python/Baekjoon/level1/level1_21_30.py
@@ -0,0 +1,29 @@
+# [2439] 별 찍기-2
+num = int(input())
+for i in range(1, num+1):
+    print(" "*(num-i) + "*"*i)
+
+# [2440] 별 찍기-3
+num = int(input())
+for i in range(num, 0, -1):
+    print("*"*i)
+
+# [2441] 별 찍기-4
+num = int(input())
+for i in range(num, 0, -1):
+    print(" "*(num-i)+"*"*i)
+
+# [2442] 별 찍기-5
+num = int(input())
+for i in range(1, num+1):
+    print(" " * (num-i)+"*"*((i*2)-1))
+
+# [2445] 별 찍기-8
+
+# [2446] 별 찍기-9
+
+# [10991] 별 찍기-16
+
+# [10992] 별 찍기-17
+
+# [1463] 1로 만들기
