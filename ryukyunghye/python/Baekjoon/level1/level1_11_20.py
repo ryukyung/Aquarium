0168320dd6929527b4e28640ad78a344eca666b6
ryukyunghye <ryukyung123@naver.com>
2022-05-22 00:02:04 +0900

    add: level1 수정

--- ryukyunghye/python/Baekjoon/level1/level1_11_20.py
+++ ryukyunghye/python/Baekjoon/level1/level1_11_20.py
@@ -39,9 +39,9 @@ m, d = map(int, input().split())
 day = 0
 dayList = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
 weekList = ["SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"]
-for i in range(m-1):  # 리스트는 0부터 시작이라 1을 빼준거임
-    day = day + dayList[i]
-day = (day+d) % 7
+for i in range(m-1):  # 리스트의 시작 : 0 -> 1월이 0번지(31), m월을 받으려면 m-1
+    day = day + dayList[i]  # day 채우기
+day = (day+d) % 7          # day + d = m월을 '일'로 바꾸고 + d, %7: 요일 받기
 print(weekList[day])
 
 # [8393] 합
