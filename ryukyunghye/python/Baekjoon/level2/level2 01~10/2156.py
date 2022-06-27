650a56ab64bcb40bdf9ececb988b0df3fbfa7039
ryukyunghye <ryukyung123@naver.com>
2022-06-27 12:06:33 +0900

    Edit(level2): 수정

--- ryukyunghye/python/Baekjoon/level2/level2 01~10/2156.py
+++ ryukyunghye/python/Baekjoon/level2/level2 01~10/2156.py
@@ -1,4 +1,5 @@
 # [2156] 포도주 시식
+# 방법1
 n = int(input())
 dp = []
 for i in range(n):
@@ -13,6 +14,7 @@ for i in range(3, n):
     list0[i] = max(list0[i-1], list0[i-3]+dp[i-1]+dp[i], list0[i-2]+dp[i])
 print(list0[n-1])
 
+# 방법2
 n = int(input())
 dp = []
 for i in range(n):
@@ -22,3 +24,17 @@ if n > 1:
     drink = dp[0]+dp[1]
 if n > 2:
     drink[2] = max(dp[2]+dp[1], dp[2]+dp[0], drink[1])
+
+# 방법3
+n = int(input())
+array = [0] * 10000
+for i in range(n):
+    array[i] = int(input())
+result = [0]*10000
+result[0] = array[0]
+result[1] = array[0]+array[1]
+result[2] = max(array[2]+result[0], array[2]+array[1], result[1])
+for i in range(3, n):
+    result[i] = max(array[i]+result[i-2], array[i] +
+                    array[i-1]+result[i-3], result[i-1])
+print(max(result))
